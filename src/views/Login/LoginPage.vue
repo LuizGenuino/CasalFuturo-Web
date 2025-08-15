5b19894245be01e4819cbc8ca6a68ba1586d74a1
genuinoLuiz <luizgustavo@pitom.com.br>
2025-08-15 13:58:00 -0400

    feat: cria função para exibir mensagem de erro da request

--- src/views/Login/LoginPage.vue
+++ src/views/Login/LoginPage.vue
@@ -9,6 +9,7 @@ import type { SignInData } from '@/types';
 import { AuthService } from '@/services';
 import { setToken, setUser } from '@/utils/storage';
 import router from '@/router';
+import { parseMessage } from '@/utils/parseMessage';
 
 const toast = useToast();
 
@@ -51,19 +52,6 @@ function validate() {
         return false;
     }
     return true;
-}
-
-function parseMessage(value: string) {
-    try {
-        const parsed = JSON.parse(value);
-        // Se o parse funcionar e existir a propriedade message
-        if (parsed && typeof parsed === 'object' && 'message' in parsed) {
-            return parsed.message;
-        }
-    } catch {
-        // Não é JSON, retorna a string original
-    }
-    return value;
 }
 
 async function onFormSubmit() {
